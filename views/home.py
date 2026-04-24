0387c73d58d8d19d4c0435e57323665ae0383dc2
Wendley Cunha <wendleycunha@gmail.com>
2026-04-24 15:58:37 -0300

    Update home.py

--- views/home.py
+++ views/home.py
@@ -1,6 +1,6 @@
 # views/home.py
 import streamlit as st
-import database as db # ou from modulos import database as db, conforme seu projeto
+from modulos import database as db # ou from modulos from modulos import database as db, conforme seu projeto
 
 def exibir(user_info):
     st.title(f"Olá, {user_info['nome']}! 👋")
